84d0a7c68d78e80bceedd69455e459029036c581
krishantKumar <80366809+krishantKumar@users.noreply.github.com>
2023-09-19 14:55:49 +1200

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- makes website responsive-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <!-- Linking CSS file -->
+
+    <link rel="stylesheet" href="css/index2.css">
+</head>
+<body>
+    <!-- navigation bar -->
+    <section class="navbar">
+        <div class="container">
+          <div class="logo">
+            <a href="index.html"><img src="images/HHSlogo.png" alt="HHS logo" class="img-responsive"></a>
+          </div>
+          
+          <div class="menu text-right">
+            
+            <ul>
+              <h1 class="nav-title">HENDERSON HIGH SCHOOL CAFE</h1>            
+              <li>
+                <a href="index.php">HOME</a>
+              </li>
+              <li>
+                <a href="menu.php">ORDER</a>
+              </li>
+              <li>
+                <a href="aboutus.php">ABOUT US</a>
+              </li>
+              <li>
+                <a href="contactus.php">CONTACT US</a>
+              </li>
+            </ul>
+        </div>
+          <div class="clearfix"></div>
+        </div>
+      </section>
+      <!-- navigation bar ends -->
+          <!-- Background -->
+    <section class="title-background1 text-center">
+        <div class="container">
+          <h1 class="title-text">CONTACT US</h1>
+        </div>
+      </section>
+      <!-- Background ends -->
+
+    <!-- navigation columns -->
+    <section class="nav-columns">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="index.html">HOME</a>
+                </li>
+                <li>
+                    <a href="menu.html">ORDER</a>
+                </li>
+                <li>
+                    <a href="aboutus.html">ABOUT US</a>
+                </li>
+                <li>
+                    <a href="contactus.html">CONTACT US</a>
+                </li>                
+            </ul>
+            <div class="clearfix"></div>
+        </div>
+        </section>
+        <section class="social-media">
+        <div class="socials">
+        <ul>
+            <li>
+                <a href="#"><img width="48" height="48" src="https://img.icons8.com/fluency/96/facebook-new.png" alt="facebook-new"/></a>
+            </li>
+            <li>
+                <a href="#"><img width="48" height="48" src="https://img.icons8.com/fluency/96/instagram-new.png" alt="instagram-new"/></a>
+            </li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+    </section>
+    <!-- navigation columns ends -->
+
+    <!-- footer -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. © Krishant Kumar</p>
+        </div>
+    </section>
+    <!-- footer ends -->
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,21 +1,22 @@
 <?php
 
 include 'config.php';
-session_start();
 
 if(isset($_POST['submit'])){
 
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
 
    $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
    if(mysqli_num_rows($select) > 0){
-      $row = mysqli_fetch_assoc($select);
-      $_SESSION['user_id'] = $row['id'];
-      header('location:index.php');
+      $message[] = 'user already exist!';
    }else{
-      $message[] = 'incorrect password or email!';
+      mysqli_query($conn, "INSERT INTO `user_form`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
+      $message[] = 'registered successfully!';
+      header('location:login.php');
    }
 
 }
@@ -28,7 +29,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>login</title>
+   <title>register</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
@@ -47,11 +48,13 @@ if(isset($message)){
 <div class="form-container">
 
    <form action="" method="post">
-      <h3>login now</h3>
+      <h3>register now</h3>
+      <input type="text" name="name" required placeholder="enter username" class="box">
       <input type="email" name="email" required placeholder="enter email" class="box">
       <input type="password" name="password" required placeholder="enter password" class="box">
-      <input type="submit" name="submit" class="btn" value="login now">
-      <p>don't have an account? <a href="register.php">register now</a></p>
+      <input type="password" name="cpassword" required placeholder="confirm password" class="box">
+      <input type="submit" name="submit" class="btn" value="register now">
+      <p>already have an account? <a href="login.php">login now</a></p>
    </form>
 
 </div>
